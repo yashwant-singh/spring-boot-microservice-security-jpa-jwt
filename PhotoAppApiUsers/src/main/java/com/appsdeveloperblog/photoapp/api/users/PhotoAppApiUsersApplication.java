0aba6fb5fb80eb37e390b47bbfa71417e669a66c
Yashwant Kumar <Yashwant_Kumar@intuit.com>
2020-02-09 21:03:18 +0530

    Fiegn client

--- PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/PhotoAppApiUsersApplication.java
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/PhotoAppApiUsersApplication.java
@@ -4,12 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
+import feign.Logger;
+
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
 public class PhotoAppApiUsersApplication {
 
 	public static void main(String[] args) {
@@ -25,5 +29,10 @@ public class PhotoAppApiUsersApplication {
 	@LoadBalanced
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
+	}
+	
+	@Bean
+	Logger.Level feignLoggerLover() {
+		return Logger.Level.FULL;
 	}
 }
--- PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/service/UsersServiceImpl.java
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/service/UsersServiceImpl.java
@@ -6,37 +6,40 @@ import java.util.UUID;
 
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.core.env.Environment;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
+import com.appsdeveloperblog.photoapp.api.users.data.AlbumsServiceClient;
 import com.appsdeveloperblog.photoapp.api.users.data.UserEntity;
 import com.appsdeveloperblog.photoapp.api.users.data.UsersRepository;
 import com.appsdeveloperblog.photoapp.api.users.shared.UserDto;
 import com.appsdeveloperblog.photoapp.api.users.ui.model.AlbumResponseModel;
+
+import feign.FeignException;
 
 @Service
 public class UsersServiceImpl implements UsersService {
 
 	private UsersRepository usersRepository;
 	private BCryptPasswordEncoder bCryptPasswordEncoder;
-	private RestTemplate restTemplate;
+	private AlbumsServiceClient albumServiceClient;
 	private Environment environment;
+
+	private Logger logger = LoggerFactory.getLogger(UsersServiceImpl.class);
 
 	@Autowired
 	public UsersServiceImpl(UsersRepository usersRepository, BCryptPasswordEncoder bCryptPasswordEncoder,
-			RestTemplate restTemplate, Environment environment) {
+			AlbumsServiceClient albumServiceClient, Environment environment) {
 		this.usersRepository = usersRepository;
 		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
-		this.restTemplate = restTemplate;
+		this.albumServiceClient = albumServiceClient;
 		this.environment = environment;
 	}
 
@@ -78,13 +81,20 @@ public class UsersServiceImpl implements UsersService {
 			throw new UsernameNotFoundException("User not found");
 		}
 		UserDto userDto = new ModelMapper().map(userEntity, UserDto.class);
-		String albumsUrl = String.format(environment.getProperty("albums.url"), userId);
-		
-		ResponseEntity<List<AlbumResponseModel>> albumsListResponse = restTemplate.exchange(albumsUrl, HttpMethod.GET,
-				null, new ParameterizedTypeReference<List<AlbumResponseModel>>() {
-				});
 
-		List<AlbumResponseModel> albumsList = albumsListResponse.getBody();
+//		String albumsUrl = String.format(environment.getProperty("albums.url"), userId);
+//		ResponseEntity<List<AlbumResponseModel>> albumsListResponse = restTemplate.exchange(albumsUrl, HttpMethod.GET,
+//				null, new ParameterizedTypeReference<List<AlbumResponseModel>>() {
+//				});
+//		List<AlbumResponseModel> albumsList = albumsListResponse.getBody();
+
+		List<AlbumResponseModel> albumsList = null;
+		try {
+			albumsList = albumServiceClient.getAlbums(userId);
+		} catch (FeignException e) {
+			logger.error(e.getMessage());
+		}
+
 		userDto.setAlbums(albumsList);
 		return userDto;
 	}
